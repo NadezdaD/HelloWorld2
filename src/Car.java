012bbe83868446485f25e5d1b733e9b69cb5e3d9
Nadezda <nadezda.simic79@gmail.com>
2019-01-27 19:57:10 +0100

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,23 @@
+public class Car {
+    
+    int maxSpeed = 180;
+    double weight = 1340.43;
+    boolean isTheCarOn = false;
+    String model = "Audi";
+    
+    double maxFuel = 100;
+    double currentFuel = 50;
+    int consumption = 5; //per 100km
+    double mileagePassed = 2000;
+    
+    public void printVariables() {
+        System.out.println("Maksimalna brzina: " + maxSpeed);
+        System.out.println("Maksimalno goriva: " + maxFuel);
+        System.out.println("Tezina: " + weight);
+        System.out.println("Model: " + model);
+        System.out.println("Radno stanje: " + isTheCarOn);
+        System.out.println("Trenutno stanje goriva: " + currentFuel);
+        System.out.println("Potrosnja: " + consumption);
+        System.out.println("Predjena kilometraza: " + mileagePassed);
+}
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Nadezda");
+        
+        String user = "Nadezda Simic";
+        System.out.println(user);
+        
+        Car audi = new Car();
+        audi.printVariables();
+        
+        }
+    
+}
